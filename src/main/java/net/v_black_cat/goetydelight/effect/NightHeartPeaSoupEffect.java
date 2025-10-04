ec5a27ee68435d747842148172035014fa25b3fc
V_BlackCAT <2049304687@qq.com>
2025-10-04 16:30:24 +0800

    碗豆汤仆从上限翻倍效果

--- src/main/java/net/v_black_cat/goetydelight/effect/NightHeartPeaSoupEffect.java
+++ src/main/java/net/v_black_cat/goetydelight/effect/NightHeartPeaSoupEffect.java
@@ -1,11 +1,18 @@
 package net.v_black_cat.goetydelight.effect;
 
+import com.Polarice3.Goety.api.items.magic.IFocus;
+import com.Polarice3.Goety.api.items.magic.IWand;
+import com.Polarice3.Goety.common.items.magic.DarkWand;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class NightHeartPeaSoupEffect extends MobEffect {
     public NightHeartPeaSoupEffect() {
@@ -36,4 +43,5 @@ public class NightHeartPeaSoupEffect extends MobEffect {
         // 每10tick执行一次效果应用
         return duration % 10 == 0;
     }
+
 }
