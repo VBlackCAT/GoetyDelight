b76254af4ee1a1dd0baa5c8911a85d1e44cda45c
V_BlackCAT <2049304687@qq.com>
2025-10-26 13:59:15 +0800

    刷子修复

--- src/main/java/net/v_black_cat/goetydelight/item/CursedIngotBrushItem.java
+++ src/main/java/net/v_black_cat/goetydelight/item/CursedIngotBrushItem.java
@@ -2,14 +2,23 @@ package net.v_black_cat.goetydelight.item;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.BrushItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BrushableBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BrushableBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.event.TickEvent;
@@ -39,44 +48,64 @@ public class CursedIngotBrushItem extends BrushItem {
         }, MAX_BRUSH_DISTANCE);
     }
 
-    @Override
     public void onUseTick(Level level, LivingEntity livingEntity, ItemStack stack, int remainingUseDuration) {
-        if (!(livingEntity instanceof Player player)) {
-            super.onUseTick(level, livingEntity, stack, remainingUseDuration);
-            return;
-        }
+        if (remainingUseDuration >= 0 && livingEntity instanceof Player player) {
+            HitResult hitresult = this.calculateHitResult(livingEntity);
+            if (hitresult instanceof BlockHitResult blockhitresult) {
+                if (hitresult.getType() == HitResult.Type.BLOCK) {
+                    int i = this.getUseDuration(stack) - remainingUseDuration + 1;
 
-        
-        super.onUseTick(level, livingEntity, stack, remainingUseDuration);
+                    //修改刷扫间隔
+                    boolean flag = i % 2 == 0;
+                    if (flag) {
+                        BlockPos blockpos = blockhitresult.getBlockPos();
+                        BlockState blockstate = level.getBlockState(blockpos);
+                        HumanoidArm humanoidarm = livingEntity.getUsedItemHand() == InteractionHand.MAIN_HAND ? player.getMainArm() : player.getMainArm().getOpposite();
+                        this.spawnDustParticles(level, blockhitresult, blockstate, livingEntity.getViewVector(0.0F), humanoidarm);
+                        Block $$18 = blockstate.getBlock();
+                        SoundEvent soundevent;
+                        if ($$18 instanceof BrushableBlock) {
+                            BrushableBlock brushableblock = (BrushableBlock)$$18;
+                            soundevent = brushableblock.getBrushSound();
+                        } else {
+                            soundevent = SoundEvents.BRUSH_GENERIC;
+                        }
 
-        if (!level.isClientSide) {
-            int i = this.getUseDuration(stack) - remainingUseDuration + 1;
+                        level.playSound(player, blockpos, soundevent, SoundSource.BLOCKS);
 
-            
-            boolean isTriggerTick = (i % 23 == 23 / 2);
+                        if (!level.isClientSide()) {
+                            BlockEntity blockentity = level.getBlockEntity(blockpos);
 
-            if (isTriggerTick) {
-                HitResult hitResult = this.calculateHitResult(player);
-                if (hitResult.getType() == HitResult.Type.BLOCK) {
-                    BlockHitResult blockHitResult = (BlockHitResult) hitResult;
-                    BlockPos pos = blockHitResult.getBlockPos();
-                    BlockEntity blockEntity = level.getBlockEntity(pos);
 
-                    if (blockEntity instanceof BrushableBlockEntity brushableEntity) {
-                        BrushableBlockEntityAccessor accessor = (BrushableBlockEntityAccessor) brushableEntity;
-                        int currentBrushCount = accessor.getBrushCount();
 
-                        
-                        int newBrushCount = Math.min(10, currentBrushCount + 1);
-                        accessor.setBrushCount(newBrushCount);
 
-                        if (newBrushCount >= 10) {
-                            level.scheduleTick(pos, brushableEntity.getBlockState().getBlock(), 1);
+                            if (blockentity instanceof BrushableBlockEntity) {
+                                BrushableBlockEntity brushableblockentity = (BrushableBlockEntity)blockentity;
+                                boolean flag1 = brushableblockentity.brush(level.getGameTime(), player, blockhitresult.getDirection());
+                                if(blockentity instanceof BrushableBlockEntityAccessor accessor){
+                                    //减少方块冷却
+                                    accessor.setCoolDownEndsAtTick((long) (accessor.getBrushCountResetsAtTick()/1.5));
+                                }
+                                if (flag1) {
+                                    EquipmentSlot equipmentslot = stack.equals(player.getItemBySlot(EquipmentSlot.OFFHAND)) ? EquipmentSlot.OFFHAND : EquipmentSlot.MAINHAND;
+                                    stack.hurtAndBreak(1, livingEntity, (p_279044_) -> {
+                                        p_279044_.broadcastBreakEvent(equipmentslot);
+                                    });
+
+                                }
+                            }
                         }
                     }
+
+                    return;
                 }
             }
+
+            livingEntity.releaseUsingItem();
+        } else {
+            livingEntity.releaseUsingItem();
         }
+
     }
     
 }
