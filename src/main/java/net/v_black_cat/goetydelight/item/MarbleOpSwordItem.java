6b76f3c303692d80dd03c9a7a47bf845511bbb19
V_BlackCAT <2049304687@qq.com>
2025-12-26 22:56:09 +0800

    随便加强一通大理石op剑庆祝一下

--- src/main/java/net/v_black_cat/goetydelight/item/MarbleOpSwordItem.java
+++ src/main/java/net/v_black_cat/goetydelight/item/MarbleOpSwordItem.java
@@ -1,12 +1,19 @@
 package net.v_black_cat.goetydelight.item;
 
+import net.minecraft.core.Holder;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 import net.v_black_cat.goetydelight.mixin.LivingEntityAccessor;
 
+import static net.minecraft.world.damagesource.DamageTypes.PLAYER_EXPLOSION;
+
 public class MarbleOpSwordItem extends SwordItem {
+    boolean YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS =true;
     public MarbleOpSwordItem(Tier tier, int attackDamageModifier, float attackSpeedModifier, Properties properties) {
         super(tier, attackDamageModifier, attackSpeedModifier, properties);
     }
@@ -15,6 +22,9 @@ public class MarbleOpSwordItem extends SwordItem {
     public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         // 正确的Accessor使用方式
         ((LivingEntityAccessor) target).setDead(true);
-        return super.hurtEnemy(stack, target, attacker);
+        target.deathTime = 0;
+        target.setHealth(0);
+        target.die(new DamageSource((attacker.damageSources().explosion( attacker,  attacker)).typeHolder(), attacker));
+        return YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS;
     }
 }
