bb66410bd5e87fb351e14260527fc11ed20ab135
VBlackCAT <2049304687@qq.com>
2026-01-03 00:41:30 +0800

    refactor(mixin): 修复幻味草渲染非开发环境不生效

--- src/main/java/net/v_black_cat/goetydelight/mixin/ItemRendererMixin.java
+++ src/main/java/net/v_black_cat/goetydelight/mixin/ItemRendererMixin.java
@@ -4,13 +4,16 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static net.v_black_cat.goetydelight.item.MetamorphicScentGrassItem.MetamorphicScentGrassRenderItem;
 
@@ -19,13 +22,13 @@ public class ItemRendererMixin {
     // 修改模型获取中的物品堆栈
     @ModifyVariable(
             method = "getModel",
-            at = @At(
-                    value = "HEAD"
-            ),
-            name = "stack",
-            remap = false
+            at = @At("HEAD"),
+            argsOnly = true,
+            index = 1
     )
     private ItemStack modifyModelItemStack(ItemStack original) {
         return MetamorphicScentGrassRenderItem(original);
     }
+
+
 }
