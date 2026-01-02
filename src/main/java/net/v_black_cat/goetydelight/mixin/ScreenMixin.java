d84b7818a2d6b18257c5763cd8e2bbed963c7974
VBlackCAT <2049304687@qq.com>
2026-01-03 00:41:30 +0800

    refactor(mixin): 修复幻味草渲染非开发环境不生效

--- src/main/java/net/v_black_cat/goetydelight/mixin/ScreenMixin.java
+++ src/main/java/net/v_black_cat/goetydelight/mixin/ScreenMixin.java
@@ -8,14 +8,13 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import static net.v_black_cat.goetydelight.item.MetamorphicScentGrassItem.MetamorphicScentGrassRenderItem;
 
-@Mixin(value = Screen.class,remap = false)
+@Mixin(Screen.class)
 public class ScreenMixin {
     @ModifyVariable(
             method = "getTooltipFromItem(Lnet/minecraft/client/Minecraft;Lnet/minecraft/world/item/ItemStack;)Ljava/util/List;",
             at = @At("HEAD"),
-            name = "item",
             argsOnly = true,
-            ordinal = 0
+            index = 1
     )
     private static ItemStack modifyMainTooltipStack(ItemStack originalStack) {
         return MetamorphicScentGrassRenderItem(originalStack);
