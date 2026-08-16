303b610e8946126de1c540d99ee88d84f111eb88
V_BlackCAT <2049304687@qq.com>
2026-08-16 23:01:05 +0800

    refactor: 深红记忆标记迁移到 FoodState attachment

--- src/main/java/net/v_black_cat/goetydelight/buff/effect/impl/CrimsonMemoriesEffect.java
+++ src/main/java/net/v_black_cat/goetydelight/buff/effect/impl/CrimsonMemoriesEffect.java
@@ -2,6 +2,7 @@ package net.v_black_cat.goetydelight.buff.effect.impl;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.v_black_cat.goetydelight.buff.effect.BuffEffect;
+import net.v_black_cat.goetydelight.init.ModAttachments;
 
 public class CrimsonMemoriesEffect implements BuffEffect {
     @Override
@@ -9,11 +10,11 @@ public class CrimsonMemoriesEffect implements BuffEffect {
 
     @Override
     public void onApply(LivingEntity entity, int amplifier) {
-        entity.getPersistentData().putBoolean("hasCrimsonMemories", true);
+        entity.getData(ModAttachments.FOOD_STATE).setCrimsonMemories(true);
     }
 
     @Override
     public void onRemove(LivingEntity entity, int amplifier) {
-        entity.getPersistentData().putBoolean("hasCrimsonMemories", false);
+        entity.getData(ModAttachments.FOOD_STATE).setCrimsonMemories(false);
     }
 }
